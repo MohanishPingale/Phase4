9e5368dd366b8c0efab30825a0b87c0e1499f38b
MohanishPingale <85016536+MohanishPingale@users.noreply.github.com>
2021-11-16 20:10:34 +0530

    Delete src/main directory

--- src/main/java/com/online/garments/deal/bean/DropdownListBean.java
+++
@@ -1,9 +0,0 @@
-package com.online.garments.deal.bean;
-
-
-public interface DropdownListBean
-{
-	public String getKey();
-
-	public String getValue();
-}
--- src/main/java/com/online/garments/deal/exception/DuplicateRecordException.java
+++
@@ -1,9 +0,0 @@
-package com.online.garments.deal.exception;
-
-public class DuplicateRecordException  extends Exception
-{
-
-	public DuplicateRecordException(String msg) {
-		super(msg);
-	}
-}
